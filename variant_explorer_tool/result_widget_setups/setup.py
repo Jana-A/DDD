1aece746d97b2efc9d9e5bc4d3b50968b0370b60
Awada <Awada@Awadas-MacBook-Air.local>
2015-10-13 20:12:45 +0100

    custome module 1

---
+++ variant_explorer_tool/result_widget_setups/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup (
+		name = 'result_widget_setups',
+		packages = ['result_widget_setups'],
+		version = '1.0',
+		author = 'Jana Awada',
+		author_email = 'awada_jana@yahoo.com',
+		url = '',
+		description = 'Various widgets for the variant explorer interface.'
+		long_description = '',
+		classifiers = [
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent'
+		]
+)
